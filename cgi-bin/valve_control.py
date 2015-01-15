579202a03589ba23c27c5890f020e772791773ac
brianh014 <brianh014@gmail.com>
2015-01-15 17:10:21 -0600

    Initial python script

---
+++ cgi-bin/valve_control.py
@@ -0,0 +1,38 @@
+import cgi
+import cgitb; cgitb.enable()
+import sys
+import RPi.GPIO as io
+from time import sleep
+io.setmode(io.BCM)
+
+print "Content-type:text/html\r\n\r\n"
+print "<html>"
+print "<head>"
+print """
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="Description" content="Site for the auto bartender">
+    <link rel="stylesheet" type="text/css" href="../style.css">
+"""
+# Create instance of FieldStorage 
+form = cgi.FieldStorage()
+order = form.getvalue('drink')
+
+print "<title>WikiBot Results</title>"
+print "</head>"
+print "<body>"
+
+print """
+    <div style="margin-left:40px; margin-right:5%%; font-size:18px; font-family:Hoefler Text, 'Times New Roman';">
+        <p>Your order %s is now being made!<p>
+    <div>
+""" % order
+
+print "</body>"
+print "</html>"
+
+v1 = 18
+
+io.setup(v1, io.OUT)
+
+io.output(v1, True)
+
