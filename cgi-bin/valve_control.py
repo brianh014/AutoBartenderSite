cb5a942e43a0ad4f0965245102acb50b3fd53a6e
brianh014 <brianh014@gmail.com>
2015-01-16 11:05:35 -0600

    Check to see if order already being made

--- cgi-bin/valve_control.py
+++ cgi-bin/valve_control.py
@@ -4,6 +4,7 @@ import cgi
 import cgitb; cgitb.enable(display=0, logdir="/path/to/logdir")
 import sys
 import pigpio
+import json
 from time import sleep
 
 print "Content-type:text/html\r\n\r\n"
@@ -18,24 +19,40 @@ print """
 form = cgi.FieldStorage()
 order = form.getvalue('drink')
 
+file = open("busy.json")
+busy = json.load(file)
+
 print "<title>Auto-Bartender</title>"
 print "</head>"
 print "<body>"
 
-print """
+if busy["busy"] == "true":
+	print """
     <div style="margin-left:40px; margin-right:5%%; font-size:18px; font-family:Hoefler Text, 'Times New Roman';">
-        <p>Your order %s is now being complete!<p>
+        <p>The auto-bartender is already making an order! Please re-order when it is not busy!<p>
     <div>
-""" % order
+	"""
+else:
+	print """
+		<div style="margin-left:40px; margin-right:5%%; font-size:18px; font-family:Hoefler Text, 'Times New Roman';">
+			<p>Your order is now complete!<p>
+		<div>
+	"""
 
 print "</body>"
 print "</html>"
 
-io = pigpio.pi()
 
-io.write(18, 1)
+if busy["busy"] == "false":
+	busy["busy"] = "true":
+	json.dump(busy, file)
+	
+	io = pigpio.pi()
+	io.write(18, 1)
+	sleep(2)
+	io.write(18, 0)
+	busy["busy"] = "false":
+	json.dump(busy, file)
+	file.close()
 
-sleep(2)
-
-io.write(18, 0)
 
